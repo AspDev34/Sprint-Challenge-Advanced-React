f87cfc92132f87d3c0f08a50641e3d725a6694b0
Ike Otu <ikeotu@msn.com>
2020-02-14 12:17:06 -0800

    data now displaying from api call

--- client/src/App.js
+++ client/src/App.js
@@ -3,47 +3,40 @@ import './App.css';
 import axios from 'axios';
 import PlayerCard from './components/PlayerCard';
 
-
-
-
 class App extends React.Component {
-  
-state = {
 
-  player: [],
+  constructor() {
+    super();
+    this.state = {
+      player: [],
+    };
+  }
+  // state = {
+  //   player: [],
+  // };
 
-};
-
-componentDidMount() {
-  axios.get("http://localhost:5000/api/players")
-    .then(res => {
-      console.log("SPRINT API DATA", res.data)
-      this.setState({
-        player: res.data
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log("SPRINT API DATA", res.data)
+        this.setState({
+          player: res.data
+        })
       })
-    })
-    .catch(error => {
-      console.log("An error has occurred", error);
-    })
-};
+      .catch(error => {
+        console.log("An error has occurred", error);
+      })
+  };
 
-render() {
+  render() {
 
-  return(
+    return (
 
-    <div>
-      <PlayerCard playerData={this.state.player}/>
-
-      hello ooooo
-
-
-
-    </div>
-
-
-  );
+      <div>
+        <PlayerCard playerData={this.state.player} />
+        hello v
+      </div>
+    );
+  }
 }
-
-}
-
 export default App;
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -5,10 +5,23 @@ const PlayerCard = props => {
     return (
 
         <div>
-            <p>{props.playerData.name}</p>
-            <p>{props.playerData.country}</p>
-            <p>{props.playerData.searches}</p>
+            {props.playerData.map(player => (
+                <div>
+                    <p>{player.name}</p>
+                    <p>{player.country}</p>
+                    <p>{player.searches}</p>
+                </div>
+            ))}
+
+
         </div>
+        
+        
+        // <div>
+        //     <p>{props.playerData.name}</p>
+        //     <p>{props.playerData.country}</p>
+        //     <p>{props.playerData.searches}</p>
+        // </div>
 
 
     );
